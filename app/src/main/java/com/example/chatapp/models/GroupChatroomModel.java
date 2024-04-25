49b62428b849fdce2c665cc353f808b71f716e32
bangd.ampere <bangd@amperecomputing.com>
2024-04-25 19:11:51 +0700

    Implement group chat

---
+++ app/src/main/java/com/example/chatapp/models/GroupChatroomModel.java
@@ -0,0 +1,119 @@
+package com.example.chatapp.models;
+
+import com.google.firebase.Timestamp;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupChatroomModel {
+    String groupChatRoomId;
+    String groupName;
+    List<String> listUserIds;
+    Timestamp lastMessageTime;
+    String lastMessageSenderId;
+    String lastMessageSenderUsername;
+    String lastMessage;
+    String lastMessageTypeName;
+    Map<String, String> mapGroupUsernames;
+
+    public GroupChatroomModel() {
+        // Required empty public constructor
+    }
+
+    public GroupChatroomModel(String groupChatRoomId, String groupName, List<String> listUserIds, Timestamp lastMessageTime, String lastMessageSenderId, String lastMessageSenderUsername, String lastMessage, String lastMessageTypeName) {
+        this.groupChatRoomId = groupChatRoomId;
+        this.groupName = groupName;
+        this.listUserIds = listUserIds;
+        this.lastMessageTime = lastMessageTime;
+        this.lastMessageSenderId = lastMessageSenderId;
+        this.lastMessageSenderUsername = lastMessageSenderUsername;
+        this.lastMessage = lastMessage;
+        this.lastMessageTypeName = lastMessageTypeName;
+    }
+
+    public void addNewUserToGroup(String userId, String username) {
+        if (mapGroupUsernames == null) {
+            mapGroupUsernames = new HashMap<>();
+        }
+        mapGroupUsernames.put(userId, username);
+    }
+
+    public void removeUserFromGroup(String userId) {
+        if (mapGroupUsernames != null) {
+            mapGroupUsernames.remove(userId);
+        }
+    }
+
+    public String getGroupChatRoomId() {
+        return groupChatRoomId;
+    }
+
+    public void setGroupChatRoomId(String groupChatRoomId) {
+        this.groupChatRoomId = groupChatRoomId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public List<String> getListUserIds() {
+        return listUserIds;
+    }
+
+    public void setListUserIds(List<String> listUserIds) {
+        this.listUserIds = listUserIds;
+    }
+
+    public Timestamp getLastMessageTime() {
+        return lastMessageTime;
+    }
+
+    public void setLastMessageTime(Timestamp lastMessageTime) {
+        this.lastMessageTime = lastMessageTime;
+    }
+
+    public String getLastMessageSenderId() {
+        return lastMessageSenderId;
+    }
+
+    public void setLastMessageSenderId(String lastMessageSenderId) {
+        this.lastMessageSenderId = lastMessageSenderId;
+    }
+
+    public String getLastMessageSenderUsername() {
+        return lastMessageSenderUsername;
+    }
+
+    public void setLastMessageSenderUsername(String lastMessageSenderUsername) {
+        this.lastMessageSenderUsername = lastMessageSenderUsername;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+
+    public String getLastMessageTypeName() {
+        return lastMessageTypeName;
+    }
+
+    public void setLastMessageTypeName(String lastMessageTypeName) {
+        this.lastMessageTypeName = lastMessageTypeName;
+    }
+
+    public Map<String, String> getMapGroupUsernames() {
+        return mapGroupUsernames;
+    }
+
+    public void setMapGroupUsernames(Map<String, String> mapGroupUsernames) {
+        this.mapGroupUsernames = mapGroupUsernames;
+    }
+}
